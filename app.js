10ffa67e7384eec632c0ff385d5a19a8732b710e
Ubaiduulah <ubaidullah5150@gmail.com>
2021-10-27 18:13:53 +0500

    Work Complete

---
+++ app.js
@@ -0,0 +1,121 @@
+                //PRACTICE START//
+//alert( "Hello")
+//var FirstName = "Ubaidullah" ;
+//var lastName = "Nadeem" ;
+//alert( FirstName )
+//alert( lastName )
+//var FirstName = "Ubaidullah" ;
+//var lastName = "Nadeem" ;
+//var fullName = FirstName + lastName
+//alert( fullName )
+//var FirstName = "Ubaidullah" ;
+//var lastName = "Nadeem" ;
+//var fullName = FirstName - lastName
+//alert( fullName )
+ //var FirstName = "Ubaidullah" ;
+// var lastName = "Nadeem" ; 
+// var age = 17 ;
+ //alert( age )
+ //alert(FirstName + lastName + age)
+ ///ADD//
+ //var num1 = 500 ;
+ //var num2 = 300 ;
+ //var add = num1 + num2
+ //alert( add )
+ //var num1 = 900 ;
+ //var num2 = 400 ;
+ //var add = num1 + num2
+ //alert( add )
+ ///SUBTRACT//
+ //var num1 = 8000 ;
+ //var num2 = 5000 ;
+ //var sub = num1 - num2
+ //alert( sub )
+ //var num1 = 1000 ;
+ //var num2 = 500 ;
+ //var sub = num1 - num2
+ //alert( sub )
+ //MULTIPLY//
+ //var num1 = 1000 ;
+ //var num2 = 500 ;
+ //var multi = num1 * num2
+ //alert( multi )
+ //var num1 = 100 ;
+ //var num2 = 50 ;
+ //var multi = num1 * num2
+ //alert( multi )
+ //DIVIDE///
+ //var num1 = 600 ;
+ //var num2 = 500 ;
+ //var div = num1 % num2
+ //alert( div )
+ //var num1 = 800 ;
+ //var num2 = 500 ;
+ //var div = num1 % num2
+ //alert( div )
+ //var a = 20; 
+ //var result = a-- + a++ - a--
+ //alert(result)
+ //var a = 30;
+ //var result = a++ + a + --a + a-- + a
+ //alert(result)
+ //var a = 15
+ //var result = a - a++ - a--
+ //alert(result)
+ //var num1 = 20 ;
+ //var num2 = 40 ;
+ //var num3 = 60 ;
+ //var num4 = 80 ;
+ //var result = num1 + num2 - num3 + (num4 * num3)
+ //alert(result)
+ //var num1 = 10 ;
+ //var num2 = 20 ;
+ //var num3 = 30 ;
+ //var num4 = 40 ;
+ //var result = num1 + num2 - num3 + (num4 * num3)
+ //alert(result)
+ //var a = 10 ;
+ //var b = a++ ;
+ //var c = a ;
+ //var result = a + b - c * (a+b-c)
+ //alert (result)
+//var num1 = +prompt("num 1");
+//var num2 = +prompt("num 2");
+//document.write("hello <br> " );
+////PRACTICE END///
+              ///MARKSHEET///
+var name = prompt("Enter your name");
+var roll = prompt("Enter your roll number");
+var standard = prompt("Enter your class");
+var math = prompt("Enter your math marks");
+var eng = prompt("Enter your eng marks");
+var urdu = prompt("Enter your urdu marks");
+var js = prompt("Enter your js marks");
+var html = prompt("Enter your html marks");
+var obtainedmarks = parseInt(math) +parseInt(eng) +parseInt(urdu) +parseInt(js) +parseInt(html);
+var percentage = obtainedmarks /500 * 100
+document.write("Name:" + name + "<br> " ); 
+document.write("Roll Number:" + roll + "<br> " );
+document.write("Class:" + standard + "<br> " );
+document.write("Total Marks:" + obtainedmarks + "<br> " );
+document.write("Percentage:" + percentage + "<br> " );
+        ///MARKSHEET DONE///
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
